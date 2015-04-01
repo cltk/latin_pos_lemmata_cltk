06f61f5d9a2210112ca2dd8c3b0b7a3d69a67e59
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-03-31 22:06:56 -0400

    rm old tar, break pos lemmata into 7 smaller files

--- break_dict.py
+++ break_dict.py
@@ -6,13 +6,14 @@ with open('cltk_latin_pos_dict.txt') as f:
 def chunks(data, size=40000):
     # http://stackoverflow.com/a/22878842
     it = iter(data)
+    file_counter = 0
     for i in range(0, len(data), size):
-        file_counter = i + 1
+        file_counter += 1
         file_name = 'pos_lemma_' + str(file_counter) + '.py'
         print(file_name)
         new_dict = {k:data[k] for k in islice(it, size)}
-        #with open(file_name, 'w') as f:
-        #    f.write(str(new_dict))
+        with open(file_name, 'w') as f:
+            f.write(str(new_dict))
 
 if __name__ == '__main__':
     chunks(whole_dict)
